37c406b4e1f427acf8e4b4caf0f2a5355ca5664e
Justin Hayes <justin.stephen.hayes@gmail.com>
2026-02-17 03:28:47 -0500

    Remove the old macro

--- src/ts/macros/index.ts
+++ src/ts/macros/index.ts
@@ -3,6 +3,5 @@ export { createSkillCheckMessage } from "./createSkillCheckMessage";
 export { fillContainerLoot } from "./fillContainerLoot";
 export { loadNamedPosition } from "./loadNamedPosition";
 export { numberTracker } from "./numberTracker";
-export { numberTracker2 } from "./numberTracker2";
 export { partyLevelCheck } from "./partyLevelCheck";
 export { saveNamedPosition } from "./saveNamedPosition";
