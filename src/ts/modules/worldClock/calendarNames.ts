82175ce889f02212b0209d8d892e5dd215c5d508
Justin Hayes <justin.stephen.hayes@gmail.com>
2026-08-10 18:24:09 -0400

    Fix weekdays

--- src/ts/modules/worldClock/calendarNames.ts
+++ src/ts/modules/worldClock/calendarNames.ts
@@ -3,40 +3,47 @@ let originalWeekdayNames: Record<string, string> | undefined;
 
 export function applyCustomCalendarNames(): void {
   originalMonthNames = { ...CONFIG.PF2E.worldClock.AR.Months };
-  originalWeekdayNames = { ...CONFIG.PF2E.worldClock.AR.Weekdays };
+  originalWeekdayNames = { ...(CONFIG.PF2E.worldClock as any).AG.Weekdays };
+  console.log("wat", CONFIG.PF2E.worldClock);
 
-  Object.assign(CONFIG.PF2E.worldClock.AR.Months, {
-    January: "January",
-    February: "February",
-    March: "March",
-    April: "April",
-    May: "May",
-    June: "June",
-    July: "July",
-    August: "August",
-    September: "September",
-    October: "October",
-    November: "November",
-    December: "December",
+  Object.assign(CONFIG.PF2E.worldClock.AR, {
+    Months: {
+      January: "January",
+      February: "February",
+      March: "March",
+      April: "April",
+      May: "May",
+      June: "June",
+      July: "July",
+      August: "August",
+      September: "September",
+      October: "October",
+      November: "November",
+      December: "December",
+    },
   });
 
-  Object.assign(CONFIG.PF2E.worldClock.AR.Weekdays, {
-    Sunday: "Sunday",
-    Monday: "Monday",
-    Tuesday: "Tuesday",
-    Wednesday: "Wednesday",
-    Thursday: "Thursday",
-    Friday: "Friday",
-    Saturday: "Saturday",
+  Object.assign((CONFIG.PF2E.worldClock as any).AG, {
+    Weekdays: {
+      Sunday: "Sunday",
+      Monday: "Monday",
+      Tuesday: "Tuesday",
+      Wednesday: "Wednesday",
+      Thursday: "Thursday",
+      Friday: "Friday",
+      Saturday: "Saturday",
+    },
   });
 }
 
 export function restoreOriginalCalendarNames(): void {
   if (!originalMonthNames) return;
-  Object.assign(CONFIG.PF2E.worldClock.AR.Months, originalMonthNames);
+  Object.assign(CONFIG.PF2E.worldClock.AR, { Months: originalMonthNames });
   originalMonthNames = undefined;
 
   if (!originalWeekdayNames) return;
-  Object.assign(CONFIG.PF2E.worldClock.AR.Weekdays, originalWeekdayNames);
+  Object.assign((CONFIG.PF2E.worldClock as any).AG, {
+    Weekdays: originalWeekdayNames,
+  });
   originalWeekdayNames = undefined;
 }
